9b2595aec0c28f7e100f3558adac352f74f682f3
Kshitij Goel <68154931+Goelksh@users.noreply.github.com>
2020-08-24 23:30:31 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,43 @@
+// This function will create a full cycle of breathing.
+function breatheCycle () {
+    ContinuousServo.spin_one_way_with_speed(AnalogPin.P0, breatheInSpeed)
+    basic.pause(breatheInTime * 1000)
+    ContinuousServo.turn_off_motor(DigitalPin.P0)
+    basic.pause(beatheInHold * 1000)
+    ContinuousServo.spin_other_way_with_speed(AnalogPin.P0, breatheOutSpeed)
+    basic.pause(breatheOutTime * 1000)
+    ContinuousServo.turn_off_motor(DigitalPin.P0)
+    basic.pause(breatheOutHold * 1000)
+}
+input.onButtonPressed(Button.A, function () {
+    emergencyStop = 0
+    for (let index = 0; index < 10; index++) {
+        if (emergencyStop == 0) {
+            breatheCycle()
+        } else {
+            break;
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    emergencyStop = 1
+})
+// Heart on start shows that microbit started.
+// Created variables for breathing time and speed.
+let breatheOutHold = 0
+let beatheInHold = 0
+let breatheOutSpeed = 0
+let breatheInSpeed = 0
+let breatheOutTime = 0
+let breatheInTime = 0
+let emergencyStop = 0
+basic.showIcon(IconNames.Heart)
+emergencyStop = 0
+breatheInTime = 1.8
+breatheOutTime = 1.2
+breatheInSpeed = 40
+breatheOutSpeed = 30
+beatheInHold = 1
+breatheOutHold = 1
+basic.pause(500)
+basic.clearScreen()
